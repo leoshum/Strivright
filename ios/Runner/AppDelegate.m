e795c618830c4e344f6e22c55b2ee2114c8f734d
“isysenko” <sip31296@gmail.com>
2019-08-16 17:19:41 +0300

    config for ios build

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,10 +1,13 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
+#import <Fabric/Fabric.h>
+#import <Crashlytics/Crashlytics.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [Fabric with:@[[Crashlytics class]]];
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
